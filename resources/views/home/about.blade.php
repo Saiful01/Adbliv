057207655a1eaf5554338c9bb309233f0e185594
memotiur <memotiur@gmail.com>
2021-11-17 18:11:59 +0600

    ..

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -4,7 +4,7 @@
             <div class="col-sm-12 text-center">
                 <div class="section-title">
                     {{-- <p class="mt-2 text-uppercase iq-fw-3 iq-ls-3">Best Ever Services</p>--}}
-                   {{-- <h2 class="title  iq-fw-8">About Us</h2>--}}
+                    {{-- <h2 class="title  iq-fw-8">About Us</h2>--}}
                 </div>
             </div>
         </div>
@@ -12,9 +12,12 @@
             <div class="col-lg-6 align-self-center wow fadeInRight"
                  style="visibility: visible; animation-name: fadeInRight;">
                 <h2 class="iq-fw-8 mb-2">Who We are</h2>
-                <p class="mb-3">The UAE-based advertising network <strong class="text-black">"Adbliv"</strong> has started operations in Bangladesh.
-                    <strong class="text-black">"Adbliv"</strong> has a commitment to work with transparency and true ad visibility for advertisers and
-                    publishers in Bangladesh.<strong class="text-black">"Adbliv"</strong> wants to bring creative and new touches to advertisements.
+                <p class="mb-3">
+                    <strong class="text-black">"Adbliv"</strong> is an ad network agency that has started operations in Bangladesh to develop an ecosystem
+                    of full-stack SSP programmatic solutions. <strong class="text-black">"Adbliv"</strong> has a commitment to work with transparency and
+                    true ad visibility for advertisers and publishers in Bangladesh. Our motto is to build an ecosystem
+                    with effective and transparent relationships between all publishers and advertisers. <strong class="text-black">"Adbliv"</strong> wants
+                    to bring creative and new touches to advertisements.
                 </p>
 
             </div>
